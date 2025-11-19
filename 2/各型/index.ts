9affb0a37583385bda798a73a4ccaa83c33233f9
peng0o0 <tomato.202504.penghc@gmail.com>
2025-11-19 17:06:42 +0900

    init 02

---
+++ 2/各型/index.ts
@@ -0,0 +1,8 @@
+let hasValue: boolean = true;
+let hasValuee: boolean = false;
+let conunt: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+let single: string = "abc";
+//幾乎跟js差不多
+// 其他地方把滑鼠放上去的話會直接顯示他的型別
